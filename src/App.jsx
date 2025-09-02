1b60bc4440c51a5d1c978bb20d8eb25b6a34cd35
Ayushman Bhattacharya <ayushman.rick007@gmail.com>
2025-09-02 20:44:29 +0530

    Added routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/home";
+import Collection from "./Pages/collection";
+import Gift from "./Pages/gift";
+import Story from "./Pages/story";
+import Login from "./Pages/login";
+import Craft from "./Pages/craft";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/gift" element={<Gift/>} />
+      <Route path="/craft" element={<Craft/>} />
+      <Route path="/collection" element={<Collection/>} />
+      <Route path="/story" element={<Story/>} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
   );
 }
